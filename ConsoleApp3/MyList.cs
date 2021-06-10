7851eab3cb89b7ec8420d843b48d4ed50afda603
John Doe <johndoe@example.com>
2021-06-10 14:03:17 +0500

    first commit

---
+++ ConsoleApp3/MyList.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class MyList<T>
+	{
+		public void Add(T value)
+		{
+			Node<T> asd = new Node<T>();
+			asd.value = value;
+			if (firstElement == null)
+			{
+				firstElement = asd;
+			}
+			else
+			{
+				asd.prev = GetLast();
+				GetLast().next = asd;
+			}
+
+
+		}
+		Node<T> firstElement;
+		Node<T> GetLast()
+		{
+			Node<T> last = firstElement;
+
+			while (last.next != null)
+			{
+				last = last.next;
+			}
+			return last;
+		}
+		public void Print()
+		{
+			Node<T> last = GetFirst();
+			while (last != null)
+			{
+				Console.WriteLine(last.value);
+				last = last.next;
+
+			}
+		}
+		public void AddFront(T value) //Выводит предыдущий элемент
+		{
+			Node<T> rtd = new Node<T>();
+			rtd.value = value;
+
+			if (firstElement == null)
+			{
+				firstElement = rtd;
+			}
+			else
+			{
+				rtd.next = GetFirst();
+				rtd.next.prev = rtd;
+			}
+		}
+		Node<T> GetFirst()
+
+		{
+			Node<T> last = firstElement;
+
+			while (last.prev != null)
+			{
+				last = last.prev;
+			}
+			return last;
+		}
+
+		public T Get(int index) //Добавляем новый элемент в конец списка
+		{
+			Node<T> node = firstElement;
+			for (int i = 0; i < index; i++)
+			{
+				node = node.next;
+			}
+			return node.value;
+		}
+
+
+
+		public void PushBack(T value) //Добавляем новый элемент в начало списка
+		{
+			Node<T> node = new Node<T>();
+			node.value = value;
+			node.next = firstElement;
+			firstElement.prev = node;
+			firstElement = node;
+		}
+
+
+
+		public void PushFront(T value) //Получаем последний элемент списка
+		{
+			Node<T> node = new Node<T>();
+			node.value = value;
+			node.next = firstElement;
+			firstElement.prev = node;
+			firstElement = node;
+		}
+
+
+
+		public void PrintForward() //Выводим значения всего списка в обратном порядке
+		{
+			MyList<int> list = new MyList<int>();
+			list.PushBack(3);
+			list.PushBack(4);
+			list.PushBack(5);
+			list.PushBack(8);
+
+			list.PushFront(99);
+			list.PushFront(98);
+			list.PushFront(97);
+
+			Console.WriteLine("Print forward");
+			list.PrintForward();
+
+			Console.WriteLine("\nPrint backward");
+			list.PrintBackward();
+		}
+
+
+
+
+		public void PrintBackward()
+		{
+			Node<T> c = GetLast();
+			while (c != null)
+			{
+				Console.WriteLine(c.value);
+				c = c.prev;
+			}
+		
+		}
+
+		public void Remove(int i)
+		{
+			List<int> x = new List<int>();
+		}
+
+		
+	}
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyList<int> ert = new MyList<int>();
+			ert.Add(45);
+			ert.Add(55);
+			ert.Add(65);
+			ert.AddFront(89);
+			ert.Print();
+			ert.PushBack(10); //Добавление элемента в начале списка
+			ert.PushFront(50);
+
+			Console.WriteLine();
+			ert.Print();
+			Console.WriteLine("\nПервый элемент списка");
+			Console.WriteLine(ert.Get(0));
+			Console.WriteLine();
+
+			Console.WriteLine();
+			ert.Print();
+			Console.WriteLine("\nПоследний элемент списка");
+			Console.WriteLine(ert.Get(4));
+			Console.WriteLine();
+
+			Console.WriteLine();
+			ert.Print();
+			Console.WriteLine("\nСписок в обратном порядке");
+			ert.PrintBackward();
+			Console.ReadKey();
+		}
+	}
+}
